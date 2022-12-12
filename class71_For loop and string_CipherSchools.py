1ae75354c1a308ae1a5d5c72dd00fab3739832c1
Mudari <118870560+Mudari@users.noreply.github.com>
2022-12-12 18:15:41 +0530

    Add files via upload

---
+++ class71_For loop and string_CipherSchools.py
@@ -0,0 +1,13 @@
+# name = "sujay"
+# for i in range(len(name)):
+#     print(name[i])
+
+# name = "sujay"
+# for i in "mohit":
+#     print(i)
+
+num = input("enter a number: ")
+total = 0
+for i in num:
+    total += int(i)
+print(total)
